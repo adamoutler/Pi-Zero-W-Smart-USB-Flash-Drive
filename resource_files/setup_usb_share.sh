46fd9c669ae19fd805e1cf9383f83fa246b3cfd0
Troy S <79487306+tds2021@users.noreply.github.com>
2021-04-11 20:10:57 -0600

    Add files via upload

--- resource_files/setup_usb_share.sh
+++ resource_files/setup_usb_share.sh
@@ -51,13 +51,13 @@ if ! grep "dwc2" /etc/modules; then
         echo "dwc2" >> /etc/modules;
 fi
 
-cp -R /home/pi/usb_share_full_install/setup/system_files/prep_* /usr/local/bin/
-cp /home/pi/usb_share_full_install/setup/system_files/monox_wifi.py /usr/local/share/monox_wifi.py
-cp /home/pi/usb_share_full_install/setup/system_files/usbshare.py /usr/local/share/usbshare.py
-chmod a+x /usr/local/share/*.py
-chmod a+x /usr/local/bin/prep_*
+sudo cp -R /home/pi/usb_share_full_install/setup/system_files/prep_* /usr/local/bin/
+sudo cp /home/pi/usb_share_full_install/setup/system_files/monox_wifi.py /usr/local/share/monox_wifi.py
+sudo cp /home/pi/usb_share_full_install/setup/system_files/usbshare.py /usr/local/share/usbshare.py
+sudo chmod a+x /usr/local/share/*.py
+sudo chmod a+x /usr/local/bin/prep_*
 
-cp /home/pi/usb_share_full_install/setup/system_files/sudoers /etc/sudoers 
+sudo cp /home/pi/usb_share_full_install/setup/system_files/sudoers /etc/sudoers 
 
 
 #
@@ -65,16 +65,16 @@ cp /home/pi/usb_share_full_install/setup/system_files/sudoers /etc/sudoers
 #
 
 if [ -d "/home/pi/USB_Share" ]; then 
-        rm -rf /home/pi/USB_Share
+        sudo rm -rf /home/pi/USB_Share
 fi
 
 if [ -d "/home/pi/.usb_share_resources" ]; then 
-        rm -rf /home/pi/.usb_share_resources
+        sudo rm -rf /home/pi/.usb_share_resources
 fi
 
-mkdir /home/pi/USB_Share
-mkdir /home/pi/.usb_share_resources
-mkdir /home/pi/USB_Share/upload
+sudo mkdir /home/pi/USB_Share
+sudo mkdir /home/pi/.usb_share_resources
+sudo mkdir /home/pi/USB_Share/upload
 
 if [ "$#" -ne 1 ]; then
         dd bs=1M if=/dev/zero of=/home/pi/USB_Share/usbdisk.img count=2048
@@ -107,16 +107,18 @@ systemctl start usbshare.service
 #
 
 sudo rm -rf /var/www/html/*
-sudo rm -rf /home/pi/.usb_share_resources/portal/*
+sudo rm -rf /home/pi/.usb_share_resources/portal
+sudo mkdir /home/pi/.usb_share_resources/portal
 
 sudo cp -R /home/pi/usb_share_full_install/portal/html_source/* /var/www/html/
 
 sudo cp -R /home/pi/usb_share_full_install/portal/html_source /home/pi/.usb_share_resources/portal
 sudo cp -R /home/pi/usb_share_full_install/portal/scripts /home/pi/.usb_share_resources/portal
 sudo cp -R /home/pi/usb_share_full_install/portal/system_files /home/pi/.usb_share_resources/portal
-
-sudo cp -R /home/pi/usb_share_full_install/setup/system_files/upgrade_usb_share /usr/local/bin/
 sudo cp -f /home/pi/usb_share_full_install/portal/current_version.txt /home/pi/.usb_share_resources/portal/current_version.txt
+
+sudo cp -R /home/pi/usb_share_full_install/setup/system_files/upgrade_usb_share /usr/local/bin/upgrade_usb_share
+
 
 sudo chmod 777 /home/pi/.usb_share_resources/portal/scripts/*
 sudo chmod 777 /usr/local/bin/upgrade_usb_share
@@ -125,38 +127,42 @@ sudo chmod -R a+r /var/www/html/*
 sudo chmod -R a+w /home/pi/.usb_share_resources/portal/*
 sudo chmod -R a+r /home/pi/.usb_share_resources/portal/*
 
-sed -i 's@upload_max_filesize = 2M@upload_max_filesize = 128M@g' /etc/php/7.3/apache2/php.ini
-sed -i 's@post_max_size = 8M@post_max_size = 128M@g' /etc/php/7.3/apache2/php.ini
+sudo sed -i 's@upload_max_filesize = 2M@upload_max_filesize = 128M@g' /etc/php/7.3/apache2/php.ini
+sudo sed -i 's@post_max_size = 8M@post_max_size = 128M@g' /etc/php/7.3/apache2/php.ini
 
-cp /home/pi/usb_share_full_install/setup/system_files/mono_x_wifi.service /etc/systemd/system
+sudo cp /home/pi/usb_share_full_install/setup/system_files/mono_x_wifi.service /etc/systemd/system
 
 #
 # Setup camera
 #
 
+if [ -d "/home/pi/.usb_share_resources/camera" ]; then 
+        sudo rm -rf /home/pi/.usb_share_resources/camera
+fi
+
 ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
 
-mkdir /home/pi/.usb_share_resources/camera
+sudo mkdir /home/pi/.usb_share_resources/camera
 cd /home/pi/.usb_share_resources/camera
-wget https://raw.githubusercontent.com/tds2021/Pi-Zero-W-Smart-USB-Flash-Drive/main/resource_files/mjpg-streamer-master.zip
-unzip -o /home/pi/.usb_share_resources/camera/mjpg-streamer-master.zip
-rm -f /home/pi/.usb_share_resources/camera/mjpg-streamer-master.zip
-mv /home/pi/.usb_share_resources/camera/mjpg-streamer-master/mjpg-streamer-experimental /home/pi/.usb_share_resources/camera/
-rm -rf /home/pi/.usb_share_resources/camera/mjpg-streamer-master/
+sudo wget https://raw.githubusercontent.com/tds2021/Pi-Zero-W-Smart-USB-Flash-Drive/main/resource_files/mjpg-streamer-master.zip
+sudo unzip -o /home/pi/.usb_share_resources/camera/mjpg-streamer-master.zip
+sudo rm -f /home/pi/.usb_share_resources/camera/mjpg-streamer-master.zip
+sudo mv /home/pi/.usb_share_resources/camera/mjpg-streamer-master/mjpg-streamer-experimental /home/pi/.usb_share_resources/camera/
+sudo rm -rf /home/pi/.usb_share_resources/camera/mjpg-streamer-master/
 
 cd /home/pi/.usb_share_resources/camera/mjpg-streamer-experimental
-make
-make install
+sudo make
+sudo make install
 
 cp /home/pi/.usb_share_resources/portal/system_files/start_camera_stream_disabled.sh /usr/local/share/start_camera_stream.sh
-chmod a+x /usr/local/share/start_camera_stream.sh
+sudo chmod a+x /usr/local/share/start_camera_stream.sh
 
-crontab /home/pi/usb_share_full_install/setup/system_files/crontab
+sudo crontab /home/pi/usb_share_full_install/setup/system_files/crontab
 
 if grep "start_x=1" /boot/config.txt; then
 	exit
 elif grep "start_x=0" /boot/config.txt; then
-        sed -i "s/start_x=0/start_x=1/g" /boot/config.txt
+        sudo sed -i "s/start_x=0/start_x=1/g" /boot/config.txt
 else 
 	echo "start_x=1"  >> /boot/config.txt
 fi
@@ -164,15 +170,15 @@ fi
 if grep "gpu_mem=" /boot/config.txt; then
 	exit
 else 
-	echo "gpu_mem=128"  >> /boot/config.txt
+	sudo echo "gpu_mem=128"  >> /boot/config.txt
 fi
 
 # 
 # change host name
 #
 
-cp /home/pi/usb_share_full_install/setup/system_files/hosts /etc/hosts
-cp /home/pi/usb_share_full_install/setup/system_files/hostname /etc/hostname
+sudo cp /home/pi/usb_share_full_install/setup/system_files/hosts /etc/hosts
+sudo cp /home/pi/usb_share_full_install/setup/system_files/hostname /etc/hostname
 
 
 #
--- resource_files/upgrade_usb_share.sh
+++ resource_files/upgrade_usb_share.sh
@@ -2,13 +2,13 @@
 # get content from create-more
 #
 
-if [ -d "/home/pi/usb_share_upgrade" ]; then 
-        rm -rf /home/pi/usb_share_upgrade
+if [ -d "/home/pi/usb_share_full_install" ]; then 
+        rm -rf /home/pi/usb_share_full_install
 fi
 
-wget https://raw.githubusercontent.com/tds2021/Pi-Zero-W-Smart-USB-Flash-Drive/main/resource_files/usb_share_upgrade.zip
-unzip -o /home/pi/usb_share_upgrade.zip
-rm -rf /home/pi/usb_share_upgrade.zip
+wget https://raw.githubusercontent.com/tds2021/Pi-Zero-W-Smart-USB-Flash-Drive/main/resource_files/usb_share_full_install.zip
+unzip -o /home/pi/usb_share_full_install.zip
+rm -rf /home/pi/usb_share_full_install.zip
 
 #
 # Setup portal
@@ -17,14 +17,14 @@ rm -rf /home/pi/usb_share_upgrade.zip
 sudo rm -rf /var/www/html/*
 sudo rm -rf /home/pi/.usb_share_resources/portal/*
 
-sudo cp -R /home/pi/usb_share_upgrade/portal/html_source/* /var/www/html/
+sudo cp -R /home/pi/usb_share_full_install/portal/html_source/* /var/www/html/
 
-sudo cp -R /home/pi/usb_share_upgrade/portal/html_source /home/pi/.usb_share_resources/portal
-sudo cp -R /home/pi/usb_share_upgrade/portal/scripts /home/pi/.usb_share_resources/portal
-sudo cp -R /home/pi/usb_share_upgrade/portal/system_files /home/pi/.usb_share_resources/portal
+sudo cp -R /home/pi/usb_share_full_install/portal/html_source /home/pi/.usb_share_resources/portal
+sudo cp -R /home/pi/usb_share_full_install/portal/scripts /home/pi/.usb_share_resources/portal
+sudo cp -R /home/pi/usb_share_full_install/portal/system_files /home/pi/.usb_share_resources/portal
 
-sudo cp -R /home/pi/usb_share_upgrade/setup/system_files/upgrade_usb_share /usr/local/bin/
-sudo cp -f /home/pi/usb_share_upgrade/portal/current_version.txt /home/pi/.usb_share_resources/portal/current_version.txt
+sudo cp -R /home/pi/usb_share_full_install/setup/system_files/upgrade_usb_share /usr/local/bin/
+sudo cp -f /home/pi/usb_share_full_install/portal/current_version.txt /home/pi/.usb_share_resources/portal/current_version.txt
 
 sudo chmod 777 /home/pi/.usb_share_resources/portal/scripts/*
 sudo chmod 777 /usr/local/bin/upgrade_usb_share
@@ -37,5 +37,5 @@ sudo chmod -R a+r /home/pi/.usb_share_resources/portal/*
 # cleanup
 #
 
-rm -rf /home/pi/usb_share_upgrade
+rm -rf /home/pi/usb_share_full_install
 rm -f /home/pi/upgrade_usb_share.sh
