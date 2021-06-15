3fecf486182da22839e2215de8cca52bf5798455
Troy S <79487306+tds2021@users.noreply.github.com>
2021-06-15 07:33:39 -0600

    Delete upgrade_usb_share_2_4_3.sh

--- resource_files/upgrade_usb_share_2_4_3.sh
+++
@@ -1,9 +0,0 @@
-cd /tmp
-wget https://raw.githubusercontent.com/tds2021/Pi-Zero-W-Smart-USB-Flash-Drive/main/resource_files/2_4_3_resources.zip
-unzip 2_4_3_resources.zip 
-rm 2_4_3_resources.zip 
-
-2_4_3_resources/upgrade.sh
-
-rm -rf 2_4_3_resources
-rm -f upgrade_usb_share.sh
